68ea9d9a0ef37d2af12df7e697adbb2fae3a6abe
BuildTools <unconfigured@null.spigotmc.org>
2020-02-14 01:04:28 -0500

    Added Docs for DataFixer and DataFixerBuilder

--- src/main/java/com/mojang/datafixers/DataFixer.java
+++ src/main/java/com/mojang/datafixers/DataFixer.java
@@ -4,8 +4,29 @@ package com.mojang.datafixers;
 
 import com.mojang.datafixers.schemas.Schema;
 
+/**
+ * The interface that provides the methods for a DataFixer
+ * 
+ * @author Mojang
+ * @version 1.0.20
+ * @since 1.0.19
+ * */
 public interface DataFixer {
+	/**
+	 * Updates a given type reference with an input and the old version and new version.
+	 * 
+	 * @param type The TypeReference of the input Type.
+	 * @param input The data to be updated.
+	 * @param version The version that corresponds to the data type.
+	 * @param newVersion The version to convert the data type to.
+	 * @return The updated value.
+	 * */
     <T> Dynamic<T> update(DSL.TypeReference type, Dynamic<T> input, int version, int newVersion);
 
+    /**
+     * Returns the schema for a given key.
+     * @param key The key with which to get the Schema
+     * @return The Schema
+     * */
     Schema getSchema(int key);
 }
--- src/main/java/com/mojang/datafixers/DataFixerBuilder.java
+++ src/main/java/com/mojang/datafixers/DataFixerBuilder.java
@@ -19,6 +19,13 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.BiFunction;
 
+/**
+ * The class used to created a DataFixer.
+ * 
+ * @author Mojang
+ * @version 1.0.20
+ * @since 1.0.19
+ * */
 public class DataFixerBuilder {
     private static final Logger LOGGER = LogManager.getLogger();
 
@@ -27,14 +34,34 @@ public class DataFixerBuilder {
     private final List<DataFix> globalList = Lists.newArrayList();
     private final IntSortedSet fixerVersions = new IntAVLTreeSet();
 
+    /**
+     * Creates a DataFixerBuilder
+     * 
+     * @param dataVersion The highest version the fixer will be able to convert up to.
+     * */
     public DataFixerBuilder(final int dataVersion) {
         this.dataVersion = dataVersion;
     }
 
+    /**
+     * Adds a schema to the list of schemas the DataFixer will be able to convert for. Use (*, CustomSchema::new).
+     * 
+     * @param version The version of the Schema.
+     * @param factory The function used to create the Schema.
+     * @return The created Schema.
+     * */
     public Schema addSchema(final int version, final BiFunction<Integer, Schema, Schema> factory) {
         return addSchema(version, 0, factory);
     }
-
+    
+    /**
+     * Adds a schema to the list of schemas the DataFixer will be able to convert for. Use (*, *, CustomSchema::new)
+     * 
+     * @param version The version of the Schema.
+     * @param subVersion The sub version of the Schema.
+     * @param factory The function that creates the Schema.
+     * @return The created Schema.
+     * */
     public Schema addSchema(final int version, final int subVersion, final BiFunction<Integer, Schema, Schema> factory) {
         final int key = DataFixUtils.makeKey(version, subVersion);
         final Schema parent = schemas.isEmpty() ? null : schemas.get(DataFixerUpper.getLowestSchemaSameVersion(schemas, key - 1));
@@ -43,10 +70,20 @@ public class DataFixerBuilder {
         return schema;
     }
 
+    /**
+     * Adds a schema to the list of schemas the DataFixer will be able to convert for.
+     * 
+     * @param schema The Schema to add.
+     * */
     public void addSchema(final Schema schema) {
         schemas.put(schema.getVersionKey(), schema);
     }
 
+    /**
+     * Adds a DataFix to the list of DataFix's for the DataFixer.
+     * 
+     * @param fix The DataFix to add.
+     * */
     public void addFixer(final DataFix fix) {
         final int version = DataFixUtils.getVersion(fix.getVersionKey());
 
@@ -59,6 +96,12 @@ public class DataFixerBuilder {
         fixerVersions.add(fix.getVersionKey());
     }
 
+    /**
+     * Builds the DataFixer. Call after all schemas and fixers have been added. (Could use: build(Executors.newSingleThreadExecutor()); )
+     * 
+     * @param executor A thread executor that is used to build the DataFixer. 
+     * @return The DataFixer.
+     * */
     public DataFixer build(final Executor executor) {
         final DataFixerUpper fixerUpper = new DataFixerUpper(new Int2ObjectAVLTreeMap<>(schemas), new ArrayList<>(globalList), new IntAVLTreeSet(fixerVersions));
 
